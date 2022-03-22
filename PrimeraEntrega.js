4e541ba57612976c6d5a7a977fa1aafcb3fe1b7f
DiegoReyesCipo <diegoreyescipo@gmail.com>
2022-03-21 22:14:14 -0300

    Segunda pre entrega

--- PrimeraEntrega.js
+++ PrimeraEntrega.js
@@ -1,40 +1,68 @@
 console.log('Primera Entrega Diego Reyes');
+const productos = [
+    {id: 1, titulo: 'Cebollita Soñador', precio: 10000, stock: 15, superficie: 200},
+    {id: 2, titulo: 'Napolitano Ganador', precio: 12000, stock: 10, superficie: 250},
+    {id: 3, titulo: 'Campeon Mexico 86', precio: 15000, stock: 10, superficie: 300},
+]
+const btn = document.getElementById('boton1')
 
-const precio = 10000;
-let interes = 1.4;
-let precioFinanciado = 0;
-let pago;
+
+btn.addEventListener( 'click', seleccionTerreno )
+function seleccionTerreno() { 
+    const {titulo, precio} = productos [0]
+    document.getElementById("opcion").innerHTML= "El valor de "+ titulo+" es $" + precio ;
+    }
+//prueba select
+
+var valorEnvio = ""
+function selecOp(valor){
+    document.getElementById("valorDeSelect").value = valor//declararle el valor del select al input
+  }
+  
+  function obtenerValor(){
+    valor = document.getElementById("valorDeSelect").value//obtener valor del input
+    valorEnvio = valor //declarar valor a la variable a usar en el ajax
+    console.log(valorEnvio)
+      }
 
 
-function formaPago() {
-    do {
-        pago = prompt("desea pagar en 1,6 o 10 pagos");
-              if (pago !== '1'&& pago !== '6' && pago !== '10'){
-            alert(' ingrese 1, 6 o 10');
-        }
+
+
+// const precio = 10000;
+// let interes = 1.4;
+// let precioFinanciado = 0;
+// let pago;
+
+
+// function formaPago() {
+//     do {
+//         pago = prompt("desea pagar en 1,6 o 10 pagos");
+//               if (pago !== '1'&& pago !== '6' && pago !== '10'){
+//             alert(' ingrese 1, 6 o 10');
+//         }
         
-    } while (pago !== '1'&& pago !=='6' && pago !=='10');
-    if (pago == 1) {
-        alert('Total a pagar por el lote '+ precio);  
-    }
-    else if (pago == 6 || pago == 10){
-        precioFinanciado = precio * interes
-        alert ('Total a pagar financiado $' + precioFinanciado)
-       // totalCuotas(precioFinanciado)
-       const valor = totalCuotas(precioFinanciado);
-       alert ('El valor de cada cuota es $' + valor)
-    }
-}
-// funcion flecha 
-const totalCuotas = precio => precio/pago
+//     } while (pago !== '1'&& pago !=='6' && pago !=='10');
+//     if (pago == 1) {
+//         alert('Total a pagar por el lote '+ precio);  
+//     }
+//     else if (pago == 6 || pago == 10){
+//         precioFinanciado = precio * interes
+//         alert ('Total a pagar financiado $' + precioFinanciado)
+//        // totalCuotas(precioFinanciado)
+//        const valor = totalCuotas(precioFinanciado);
+//        alert ('El valor de cada cuota es $' + valor)
+//     }
+// }
+// // funcion flecha 
+// const totalCuotas = precio => precio/pago
 
-formaPago()
-/*function totalCuotas(precio) {
-    const result = precio/pago
-    alert ('El valor de cada cuota es $' + result)
-}
+// formaPago()
+// /*function totalCuotas(precio) {
+//     const result = precio/pago
+//     alert ('El valor de cada cuota es $' + result)
+// }
 
-formaPago()*/ 
+// formaPago()*/ 
      
       
     
@@ -42,42 +70,42 @@ formaPago()*/
 
 
 
-// Se incorpora el objeto/array calles para cumplir con la consigna del desafio complementario 2
-var calles = [];
-var esSalir = false
+// // Se incorpora el objeto/array calles para cumplir con la consigna del desafio complementario 2
+// var calles = [];
+// var esSalir = false
 
-const agregarCalles = () => {
+// const agregarCalles = () => {
 
-    do {
-        let calle = prompt(`Ingrese el nombre de una calle, escriba salir para saltar este paso`).toLowerCase()
-        if(calle === 'salir'){
-            esSalir = true   
-                } 
+//     do {
+//         let calle = prompt(`Ingrese el nombre de una calle, escriba salir para saltar este paso`).toLowerCase()
+//         if(calle === 'salir'){
+//             esSalir = true   
+//                 } 
 
-        calles.push(calle)
-        if (calles.length ===3 ) {
-            for (const xx of calles) {
-                console.log(`usted eligio la calle ${xx}`)
-            }
-            break
-        }
-    } while (!esSalir);
-    if (esSalir){
+//         calles.push(calle)
+//         if (calles.length ===3 ) {
+//             for (const xx of calles) {
+//                 console.log(`usted eligio la calle ${xx}`)
+//             }
+//             break
+//         }
+//     } while (!esSalir);
+//     if (esSalir){
 
-        alert('Otros usuarios seleccionaran los nombres')        
-    } else {
+//         alert('Otros usuarios seleccionaran los nombres')        
+//     } else {
 
-        alert('Los 3 nombres de calle que elejiste son: ' + calles)
-        alert ('Las opciones que hayan sido mas votadas seran las seleccionadas')
-        alert('probar git')
-    }
+//         alert('Los 3 nombres de calle que elejiste son: ' + calles)
+//         alert ('Las opciones que hayan sido mas votadas seran las seleccionadas')
+//         alert('probar git')
+//     }
     
-}
+// }
 
-//console.log(calles)
-//no se porque no puedo acceder al primer elemento del array por medio del indice
+// //console.log(calles)
+// //no se porque no puedo acceder al primer elemento del array por medio del indice
 
-agregarCalles()
-//funciono haciendolo despues de la funcion
-console.log( calles [0] +' Participara en la eleccion de avenida principal')
+// agregarCalles()
+// //funciono haciendolo despues de la funcion
+// console.log( calles [0] +' Participara en la eleccion de avenida principal')
 
